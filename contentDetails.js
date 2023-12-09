3a2e0b5573af7ae13b9337a640c40e7a33fb3a4b
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-12-09 17:28:28 +0530

    added product container css

--- contentDetails.js
+++ contentDetails.js
@@ -143,5 +143,5 @@ let httpRequest = new XMLHttpRequest()
     }
 }
 
-httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/'+id, true)
+httpRequest.open('GET', 'https://dummyjson.com/product/'+id, true)
 httpRequest.send()  
